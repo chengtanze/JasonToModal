efb68da3fba6955f951320d76507a1ce7a8e2b7d
ciwong-huanghg <Johnson@JohnsontekiMac-mini.local>
2015-07-30 10:04:13 -0700

    Initial Commit

---
+++ JasonToModal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JasonToModal
+//
+//  Created by ciwong-huanghg on 15/7/30.
+//  Copyright (c) 2015年 Hype. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JasonToModal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JasonToModal
+//
+//  Created by ciwong-huanghg on 15/7/30.
+//  Copyright (c) 2015年 Hype. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
